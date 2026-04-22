0f13163f3ba80e141873a649b81bee2afff3b888
gelexxi <schew4uk98@gmail.com>
2026-04-22 23:58:55 +0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -2,10 +2,13 @@ from django.db.models import QuerySet
 from db.models import Movie
 
 
-def get_movies(genres_ids: list[int] = None, actors_ids: list[int] = None) -> QuerySet[Movie]:
+def get_movies(genres_ids: list[int] = None,
+               actors_ids: list[int] = None
+               ) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
-    if  genres_ids and actors_ids:
-        queryset = queryset.filter(genres__id__in=genres_ids, actors__id__in=actors_ids)
+    if genres_ids and actors_ids:
+        queryset = queryset.filter(genres__id__in=genres_ids,
+                                   actors__id__in=actors_ids)
     elif genres_ids:
         queryset = queryset.filter(genres__id__in=genres_ids)
     elif actors_ids:
--- services/movie_session.py
+++ services/movie_session.py
@@ -1,14 +1,19 @@
 from django.db.models import QuerySet
-
 from db.models import MovieSession
 
-def create_movie_session(movie_show_time, movie_id, cinema_hall_id) -> MovieSession:
+
+def create_movie_session(movie_show_time: str,
+                         movie_id: int,
+                         cinema_hall_id: int
+                         ) -> MovieSession:
+
     new_movie_session = MovieSession.objects.create(
         show_time=movie_show_time,
         cinema_hall_id=cinema_hall_id,
         movie_id=movie_id
     )
     return new_movie_session
+
 
 def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
     queryset = MovieSession.objects.all()
@@ -17,8 +22,11 @@ def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
             show_time__date=session_date
         )
     return queryset
+
+
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
+
 
 def update_movie_session(session_id: int,
                          show_time: str = None,
@@ -33,7 +41,6 @@ def update_movie_session(session_id: int,
     if cinema_hall_id is not None:
         session.cinema_hall_id = cinema_hall_id
     session.save()
-
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
