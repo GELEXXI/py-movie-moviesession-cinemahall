3307bed34c5fd3b10d135cc117ad8504e1929e08
gelexxi <schew4uk98@gmail.com>
2026-04-25 21:00:35 +0300

    Solution5

--- services/movie.py
+++ services/movie.py
@@ -6,12 +6,9 @@ def get_movies(genres_ids: list[int] = None,
                actors_ids: list[int] = None
                ) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
-    if genres_ids and actors_ids:
-        queryset = queryset.filter(genres__id__in=genres_ids,
-                                   actors__id__in=actors_ids)
-    elif genres_ids:
+    if genres_ids:
         queryset = queryset.filter(genres__id__in=genres_ids)
-    elif actors_ids:
+    if actors_ids:
         queryset = queryset.filter(actors__id__in=actors_ids)
     return queryset
 
